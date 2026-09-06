f5a0743ead8bfc8321c5fd187859c49aa8dafeac
faganjohn <75076607+faganjohn@users.noreply.github.com>
2026-09-06 17:08:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ function draw() {
 		if(song1_status == false)
 		{
 			song1.play();
-			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+			document.getElementById("song").innerHTML = "Playing - XENESR DANCE SONG"
 		}
 	}
 
@@ -85,7 +85,7 @@ function draw() {
 		if(song2_status == false)
 		{
 			song2.play();
-			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+			document.getElementById("song").innerHTML = "Playing - Spectre by Alan walker"
 		}
 	}
 
